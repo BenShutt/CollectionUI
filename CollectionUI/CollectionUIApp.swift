e53fc5a2507a734a1f0ffc92322fa0c69ffaec1d
Ben Shutt <ben.c.shutt@gmail.com>
2024-06-02 16:29:19 +0100

    Initial Commit

---
+++ CollectionUI/CollectionUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionUIApp.swift
+//  CollectionUI
+//
+//  Created by Ben Shutt on 02/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct CollectionUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
